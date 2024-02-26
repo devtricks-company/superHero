4575875b24f8df401935ee8a26b16a3d42f80798
devtricks-company <amir.mo.azarbashi@gmail.com>
2024-02-27 00:44:49 +0330

    server side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import favoriteContext from "./favoriteContext";
@@ -12,38 +12,35 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true,
     },
   },
 });
 const App = () => {
   const favoriteList = useState([]);
   return (
-    <BrowserRouter>
-      <favoriteContext.Provider value={favoriteList}>
-        <QueryClientProvider client={queryClient}>
-          <Suspense
-            fallback={
-              <div>
-                <h2 style={{ color: "white" }}>Loading</h2>
-              </div>
-            }
-          >
-            <header>
-              <h1>Superhero Website</h1>
-            </header>
-            <Routes>
-              <Route path="/" element={<SearchHero />} />
-              <Route path="/details">
-                <Route path=":id" element={<Details />} />
-              </Route>
-            </Routes>
-          </Suspense>
-        </QueryClientProvider>
-      </favoriteContext.Provider>
-    </BrowserRouter>
+    <favoriteContext.Provider value={favoriteList}>
+      <QueryClientProvider client={queryClient}>
+        <Suspense
+          fallback={
+            <div>
+              <h2 style={{ color: "white" }}>Loading</h2>
+            </div>
+          }
+        >
+          <header>
+            <h1>Superhero Website</h1>
+          </header>
+          <Routes>
+            <Route path="/" element={<SearchHero />} />
+            <Route path="/details">
+              <Route path=":id" element={<Details />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </QueryClientProvider>
+    </favoriteContext.Provider>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
